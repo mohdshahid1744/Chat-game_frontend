630adfd307926426b29cdf3ae4ae0b82caa0990e
Muhammed shahid <141304399+mohdshahid1744@users.noreply.github.com>
2025-04-10 22:19:53 +0530

    Update Axios.ts

--- src/utils/Axios/Axios.ts
+++ src/utils/Axios/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://chat-game-backend.onrender.com";
+const API_BASE_URL = "http://localhost:3001";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
